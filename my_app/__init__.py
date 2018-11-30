a13b2f985f771c78ed88b287f9a4bd515a607d41
erik <mastersesin>
2018-11-30 22:04:40 +0700

    Add Secret Key

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:st!xadminHY@11.11.11.9/new_schema'
+app.config['SECRET_KEY'] = 'trantrongtyckiuzk4ever!@#!!!@@##!*&%^$$#$'
 db = SQLAlchemy(app)
 from my_app.trendview.views import trendviewApp
 app.register_blueprint(trendviewApp)
