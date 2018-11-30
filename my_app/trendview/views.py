1202d9d32840595302cd577ac640c2a06177535c
erik <mastersesin>
2018-11-30 20:36:43 +0700

    Module Register v1.0

--- my_app/trendview/views.py
+++ my_app/trendview/views.py
@@ -3,6 +3,9 @@ from my_app import app, db
 from my_app.trendview.models import User
 trendviewApp = Blueprint('trendview', __name__)
 
+returnMsg = {
+    "registerSuccess":{"code":1,"msg":"User created"}
+}
 
 @trendviewApp.route('/register', methods=['POST'])
 def register():
@@ -10,7 +13,19 @@ def register():
     email = request.form.get('email')
     telephone = request.form.get('telephone')
     password = request.form.get('password')
-    newUser = User(username,email,telephone,password)
-    db.session.add(newUser)
-    db.session.commit()
-    return "Hello"
+    duplicateUsernameCheck = User.query.filter_by(username=username).first()
+    if duplicateUsernameCheck == None:
+        duplicateEmailCheck = User.query.filter_by(email=email).first()
+        if duplicateEmailCheck == None:
+            duplicatePhoneNumberCheck = User.query.filter_by(telephone=telephone).first()
+            if duplicatePhoneNumberCheck == None:
+                newUser = User(username, email, telephone, password)
+                db.session.add(newUser)
+                db.session.commit()
+                return jsonify(returnMsg['registerSuccess'])
+            else:
+                return "Phone Duplicated"
+        else:
+            return "Email Duplicated"
+    else:
+        return "Username Duplicated"
