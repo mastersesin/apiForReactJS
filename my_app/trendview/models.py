039c4682098b91a45f9be68e78175fe7d68d8e30
erik <mastersesin>
2018-12-01 20:17:37 +0700

    Add more user info

--- my_app/trendview/models.py
+++ my_app/trendview/models.py
@@ -7,16 +7,19 @@ class User(db.Model):
     email = db.Column(db.String(255))
     telephone = db.Column(db.String(255))
     password = db.Column(db.String(255))
+    point = db.Column(db.Integer)
+    referer = db.Column(db.String(255))
 
-    def __init__(self, username, email, telephone, password):
+    def __init__(self, username, email, telephone, password, referer):
         self.username = username
         self.email = email
         self.telephone = telephone
         self.password = password
-
+        self.point = 0
+        self.referer = referer
     def __repr__(self):
         return '<User %d>' % self.id
 
     def generate_auth_token(self, expiration=600):
         s = Serializer(app.config['SECRET_KEY'], expires_in=60*60*2)
-        return s.dumps({'id': self.id})
+        return (s.dumps({'id': self.id})).decode("utf-8")
