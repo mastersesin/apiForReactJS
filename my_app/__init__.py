9bfdda10b0f799d9f82b2ca5b7664b310100e3ed
erik <mastersesin>
2018-12-01 17:59:03 +0700

    Add CORS

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:st!xadminHY@11.11.11.9/new_schema'
 app.config['SECRET_KEY'] = 'trantrongtyckiuzk4ever!@#!!!@@##!*&%^$$#$'
 db = SQLAlchemy(app)
 from my_app.trendview.views import trendviewApp
 app.register_blueprint(trendviewApp)
+CORS(trendviewApp)
 db.create_all()
